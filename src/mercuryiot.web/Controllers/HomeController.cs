035b261f9120430e3aa48d379f012c00fbbc24ba
Michael Henderson <mohenderson@outlook.com>
2020-04-26 10:27:56 -0400

    Added boilerplate razor views.

--- src/mercuryiot.web/Controllers/HomeController.cs
+++ src/mercuryiot.web/Controllers/HomeController.cs
@@ -2,23 +2,33 @@ using System;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace Mercuryiot.Web.Controllers
 {
-  public class HomeController : Controller
-  {
-
-    ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-      _logger = logger;
-    }
+        ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-      
-      return View();
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // TODO Remove
+            await Task.Yield();
+
+            return View();
+        }
+
+        public async Task<IActionResult> Error()
+        {
+            // TODO Remove
+            await Task.Yield();
+
+            return View();
+        }
     }
-  }
 }
