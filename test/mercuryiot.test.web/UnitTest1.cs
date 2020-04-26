8e368c795139813a70323c4806b8d16fde468baf
Michael Henderson <mohenderson@outlook.com>
2020-04-26 18:03:42 -0400

    Added test project.

---
+++ test/mercuryiot.test.web/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace mercuryiot.test.web
+{
+    public class UnitTest1
+    {
+        [Fact(DisplayName = "First Test")]
+        public void Test1()
+        {
+            Assert.True(false);
+        }
+    }
+}
