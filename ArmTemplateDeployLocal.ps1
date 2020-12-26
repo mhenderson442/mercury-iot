71817b23e161b10cbea02b77983b6049cb514a41
mhenderson442 <mike@mohenderson.com>
2020-12-26 12:13:35 -0500

    Troubleshooting IoT Hub private endpint template.

---
+++ ArmTemplateDeployLocal.ps1
@@ -0,0 +1,14 @@
+﻿$resourceGroupName = "venus-iot"
+
+$today=Get-Date -Format "yyyyMMddhhmmss"
+$deploymentName = $resourceGroupName + "-deployment-" + $today
+
+$templateFile = "./iot-template.json"
+$templateParameterFile = "./iot-template.parameters.json"
+
+New-AzResourceGroupDeployment `
+-Name $deploymentName `
+-ResourceGroupName $resourceGroupName `
+-Mode "Incremental" `
+-TemplateParameterFile $templateParameterFile `
+-TemplateFile $templateFile
