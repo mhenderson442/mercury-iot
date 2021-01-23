fddda475d3342c5d7762e2b5067baca7fd34d7c5
mhenderson442 <mike@mohenderson.com>
2021-01-22 20:19:08 -0500

    template edits and additions.

--- ArmTemplateDeployLocal.ps1
+++ ArmTemplateDeployLocal.ps1
@@ -3,8 +3,8 @@
 $today=Get-Date -Format "yyyyMMddhhmmss"
 $deploymentName = $resourceGroupName + "-deployment-" + $today
 
-$templateFile = "./templates/resource-groups/mars-resource-group.json"
-$templateParameterFile = ".tempaltes/resource-groups/mars-resource-group.parameters.json"
+$templateFile = "templates/resource-groups/mars-resource-group.json"
+$templateParameterFile = "templates/resource-groups/mars-resource-group.parameters.json"
 
 New-AzResourceGroupDeployment `
 -Name $deploymentName `
